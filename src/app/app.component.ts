a28267227550443b415704d0469e9233bc2f310f
bxste <97215500+bxste@users.noreply.github.com>
2023-03-02 12:40:12 +0800

    2 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, VERSION } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  todaydate;
+  constructor(private myservice: MyserviceService) {}
+  ngOnInit() {
+    this.todaydate = this.myservice.showTodayDate();
+  }
 }
---
+++ src/app/new-cmp/new-cmp.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-cmp',
+  templateUrl: './new-cmp.component.html',
+  styleUrls: ['./new-cmp.component.css'],
+})
+export class NewCmpComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
